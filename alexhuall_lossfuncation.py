b4099cf288ea98a8c7b8229c534e52d07a9f391c
alexhu <49564272+alexhuooxx@users.noreply.github.com>
2020-06-27 18:25:35 +0800

    Update alexhuall_lossfuncation.py

--- alexhuall_lossfuncation.py
+++ alexhuall_lossfuncation.py
@@ -129,37 +129,6 @@ class time_reinforce_loss(nn.Module):
 
 
 
-class Gradient_Loss(nn.Module):
-    def __init__(self,alpha,channels):
-        super(Gradient_Loss,self).__init__()
-        self.alpha=alpha
-        filter=torch.FloatTensor([[-1.,1.]]).cuda()
-
-        self.filter_x = filter.view(1,1,1,2).repeat(1,channels,1,1)
-        self.filter_y = filter.view(1,1,2,1).repeat(1,channels,1,1)
-
-
-    def forward(self, gen_frames,gt_frames):
-        # pos=torch.from_numpy(np.identity(channels,dtype=np.float32))
-        # neg=-1*pos
-        # filter_x=torch.cat([neg,pos]).view(1,pos.shape[0],-1)
-        # filter_y=torch.cat([pos.view(1,pos.shape[0],-1),neg.vew(1,neg.shape[0],-1)])
-        gen_frames_x=nn.functional.pad(gen_frames,(1,0,0,0))
-        gen_frames_y=nn.functional.pad(gen_frames,(0,0,1,0))
-        gt_frames_x=nn.functional.pad(gt_frames,(1,0,0,0))
-        gt_frames_y=nn.functional.pad(gt_frames,(0,0,1,0))
-
-        gen_dx=nn.functional.conv2d(gen_frames_x,self.filter_x)
-        gen_dy=nn.functional.conv2d(gen_frames_y,self.filter_y)
-        gt_dx=nn.functional.conv2d(gt_frames_x,self.filter_x)
-        gt_dy=nn.functional.conv2d(gt_frames_y,self.filter_y)
-
-        grad_diff_x=torch.abs(gt_dx-gen_dx)
-        grad_diff_y=torch.abs(gt_dy-gen_dy)
-
-        return torch.mean(grad_diff_x**self.alpha+grad_diff_y**self.alpha)
-
-
 class WeightedCrossEntropyLoss(nn.Module):
 
 
@@ -266,8 +235,6 @@ class FocalLoss(nn.Module):
         else:
 """
 
-
-#台湾人的bmase？
 class Loss():
 
     def __init__(self, args):
@@ -407,12 +374,7 @@ class Bmsemae(nn.Module):
 
 
 
-##### set the rader echo color bar####
-colorbar_dir = '/media/workdir/hujh/hujh-new/rader-baseline-alexhumaster/colorbar.txt'
-rgb=np.loadtxt(colorbar_dir,delimiter=',')
-rgb/=255.0
-icmap=colors.ListedColormap(rgb,name='my_color')
-cmap_color=icmap
+
 def draw_video(output_np11,target_np11,colorbar,savepath,model_name,epoch,indx):
     """
     this def is draw the compare pic when process in the training or validating
